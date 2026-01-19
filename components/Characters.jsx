e109023982df6c1888333829c6f810f9276abc9a
ELise <elise.p87@gmail.com>
2026-01-19 10:41:14 +0100

    Mise à jour page de login

--- components/Characters.jsx
+++ components/Characters.jsx
@@ -194,7 +194,6 @@ export default function Characters() {
             )}
           </motion.div>
         ) : (
-          <>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
               <AnimatePresence>
                 {filteredCharacters.map((character) => (
