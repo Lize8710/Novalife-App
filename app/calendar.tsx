cbb07cde126503ad07cd25674edfc28629f014d0
ELise <elise.p87@gmail.com>
2026-01-20 22:34:51 +0100

    Mise à jour page de login

--- app/calendar.tsx
+++ app/calendar.tsx
@@ -1,6 +1,8 @@
+
 import React from "react";
 import WeekCalendarFuturist from "../components/WeekCalendarFuturist";
 
+export default function CalendarPage() {
   return (
     <main className="min-h-screen bg-gradient-to-br from-[#0fffc1] via-[#3a8fff] to-[#1a1a40] flex flex-col items-center justify-center">
       <WeekCalendarFuturist />
