5049c8102c7219cee1611bc4114f35a48094e7a0
ELise <elise.p87@gmail.com>
2026-01-20 02:10:52 +0100

    Test redeploiement API /api/characters

--- pages/api/characters/index.ts
+++ pages/api/characters/index.ts
@@ -1,6 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import clientPromise from '../../../lib/mongodb';
 
+console.log('API /api/characters appelée - test redeploiement');
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
     try {
